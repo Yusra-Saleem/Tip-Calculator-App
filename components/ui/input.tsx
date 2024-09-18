752f50bc9b38d733aab456f2cf32b0816d693fd0
YUSRA SALEEM <159941826+Yusra-Saleem@users.noreply.github.com>
2024-09-19 04:43:33 +0500

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,9 +1,8 @@
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
 export const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type = "text", ...props }, ref) => {
     return (
       <input
         type={type}
@@ -17,3 +16,6 @@ export const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttribute
     );
   }
 );
+
+// Adding display name for the component
+Input.displayName = "Input";
